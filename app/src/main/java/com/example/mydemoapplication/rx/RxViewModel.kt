42fc7832cb0bb072f37e4186687eab23c6283518
jicheng <litchicoder@glazero.com>
2023-02-14 20:30:42 +0800

    feature:创建了各类subject

--- app/src/main/java/com/example/mydemoapplication/rx/RxViewModel.kt
+++ app/src/main/java/com/example/mydemoapplication/rx/RxViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.mydemoapplication.rx
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.mydemoapplication.data.LoginRepository
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.internal.operators.observable.ObservableCount
+import io.reactivex.rxjava3.subjects.*
 import java.util.concurrent.TimeUnit
 
 /**
@@ -15,14 +18,32 @@ import java.util.concurrent.TimeUnit
  */
 class RxViewModel() : ViewModel() {
 
+    val TAG = "RxViewModel"
+
+    private var dispose: Disposable? = null
+    private var asyncSubject = AsyncSubject.create<Int>()
+    private var behaviorSubject = BehaviorSubject.create<Int>()
+    private var completableSubject = CompletableSubject.create()
+    private var maybeSubject = MaybeSubject.create<Int>()
+    private var publishSubject = PublishSubject.create<Int>()
+    private var replaySubject = ReplaySubject.create<Int>()
+
+    //    private var serializedSubject = SerializedSubject()
+    private var singleSubject = SingleSubject.create<Int>()
+    private var unicastSubject = UnicastSubject.create<Int>()
 
     init {
-
-        Observable.interval(1, TimeUnit.SECONDS)
+        dispose = Observable.interval(1, TimeUnit.SECONDS)
             .doOnNext {
-
+                Log.d(TAG, "interval -> $it")
             }
             .subscribe()
-     }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        dispose?.dispose()
+    }
+
 
 }
