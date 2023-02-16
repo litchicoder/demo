a992dc346196aeb239d013c781f459b3c6427d26
jicheng <litchicoder@glazero.com>
2023-02-16 20:52:41 +0800

    feature:各种subject的例子

--- app/src/main/java/com/example/mydemoapplication/ScrollingActivity.kt
+++ app/src/main/java/com/example/mydemoapplication/ScrollingActivity.kt
@@ -1,33 +1,205 @@
 package com.example.mydemoapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.example.mydemoapplication.databinding.ActivityScrollingBinding
+import com.example.mydemoapplication.R
 import com.example.mydemoapplication.rx.RxViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.subjects.PublishSubject
+import io.reactivex.rxjava3.subjects.Subject
 
 class ScrollingActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityScrollingBinding
 
     private var rxViewModel: RxViewModel? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityScrollingBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_scrolling)
         rxViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory()).get(
             RxViewModel::class.java
         )
         setSupportActionBar(findViewById(R.id.toolbar))
-        binding.toolbarLayout.title = title
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
+
+        init()
+    }
+
+    private fun init() {
+        rxViewModel?.observerPublishSubject()
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe(object : Observer<Long> {
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onNext(t: Long) {
+                    findViewById<TextView>(R.id.subject_message).text =
+                        findViewById<TextView>(R.id.subject_message).text.toString() + "->" + t
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+
+                override fun onComplete() {
+                    findViewById<TextView>(R.id.subject_message).text =
+                        findViewById<TextView>(R.id.subject_message).text.toString() + "->END"
+                }
+
+
+            })
+    }
+
+    fun publish_subject(view: View) {
+        rxViewModel?.observerPublishSubject()
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe(object : Observer<Long> {
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onNext(t: Long) {
+                    findViewById<TextView>(R.id.publish_subject).text =
+                        findViewById<TextView>(R.id.publish_subject).text.toString() + "->" + t
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+
+                override fun onComplete() {
+                    findViewById<TextView>(R.id.publish_subject).text =
+                        findViewById<TextView>(R.id.publish_subject).text.toString() + "->END"
+                }
+
+
+            })
+
+//        rxViewModel?.observerPublishSubject()
+//            ?.observeOn(AndroidSchedulers.mainThread())
+//            ?.subscribe(object : Subject<Long>() {
+//                override fun subscribeActual(observer: Observer<in Long>) {
+//                }
+//
+//                override fun onSubscribe(d: Disposable) {
+//                }
+//
+//                override fun onError(e: Throwable) {
+//                }
+//
+//                override fun onComplete() {
+//                }
+//
+//                override fun hasObservers(): Boolean {
+//                    return false
+//                }
+//
+//                override fun hasThrowable(): Boolean {
+//                    return false
+//                }
+//
+//                override fun hasComplete(): Boolean {
+//                    return false
+//                }
+//
+//                override fun getThrowable(): Throwable? {
+//                    return null
+//                }
+//
+//                override fun onNext(t: Long) {
+//                    findViewById<TextView>(R.id.publish_subject).text =
+//                        findViewById<TextView>(R.id.publish_subject).text.toString() + "->" + t
+//                }
+//
+//
+//            })
+    }
+
+    fun async_subject(view: View) {
+        rxViewModel?.observerAsyncSubject()
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe(object : Observer<Long> {
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onNext(t: Long) {
+                    findViewById<TextView>(R.id.async_subject).text =
+                        findViewById<TextView>(R.id.async_subject).text.toString() + "->" + t
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+
+                override fun onComplete() {
+                    findViewById<TextView>(R.id.async_subject).text =
+                        findViewById<TextView>(R.id.async_subject).text.toString() + "->END"
+                }
+
+
+            })
+    }
+
+    fun behavior_subject(view: View) {
+        rxViewModel?.observerBehaviorSubject()
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe(object : Observer<Long> {
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onNext(t: Long) {
+                    findViewById<TextView>(R.id.behavior_subject).text =
+                        findViewById<TextView>(R.id.behavior_subject).text.toString() + "->" + t
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+
+                override fun onComplete() {
+                    findViewById<TextView>(R.id.behavior_subject).text =
+                        findViewById<TextView>(R.id.behavior_subject).text.toString() + "->END"
+                }
+
+
+            })
+    }
+
+    fun replay_subject(view: View) {
+        rxViewModel?.observerReplaySubject()
+            ?.observeOn(AndroidSchedulers.mainThread())
+            ?.subscribe(object : Observer<Long> {
+                override fun onSubscribe(d: Disposable) {
+
+                }
+
+                override fun onNext(t: Long) {
+                    findViewById<TextView>(R.id.replay_subject).text =
+                        findViewById<TextView>(R.id.replay_subject).text.toString() + "->" + t
+                }
+
+                override fun onError(e: Throwable) {
+
+                }
+
+                override fun onComplete() {
+                    findViewById<TextView>(R.id.replay_subject).text =
+                        findViewById<TextView>(R.id.replay_subject).text.toString() + "->END"
+                }
+
+
+            })
     }
 }
--- app/src/main/java/com/example/mydemoapplication/rx/RxViewModel.kt
+++ app/src/main/java/com/example/mydemoapplication/rx/RxViewModel.kt
@@ -21,21 +21,33 @@ class RxViewModel() : ViewModel() {
     val TAG = "RxViewModel"
 
     private var dispose: Disposable? = null
-    private var asyncSubject = AsyncSubject.create<Int>()
-    private var behaviorSubject = BehaviorSubject.create<Int>()
+    private var asyncSubject = AsyncSubject.create<Long>()
+    private var behaviorSubject = BehaviorSubject.create<Long>()
     private var completableSubject = CompletableSubject.create()
-    private var maybeSubject = MaybeSubject.create<Int>()
-    private var publishSubject = PublishSubject.create<Int>()
-    private var replaySubject = ReplaySubject.create<Int>()
+    private var publishSubject = PublishSubject.create<Long>()
+    private var replaySubject = ReplaySubject.create<Long>()
 
-    //    private var serializedSubject = SerializedSubject()
-    private var singleSubject = SingleSubject.create<Int>()
-    private var unicastSubject = UnicastSubject.create<Int>()
+    private var maybeSubject = MaybeSubject.create<Long>()
+    private var singleSubject = SingleSubject.create<Long>()
+
+    private var unicastSubject = UnicastSubject.create<Long>()
 
     init {
         dispose = Observable.interval(1, TimeUnit.SECONDS)
             .doOnNext {
-                Log.d(TAG, "interval -> $it")
+                Log.d(TAG, "Longerval -> $it")
+                publishSubject.onNext(it)
+                asyncSubject.onNext(it)
+                behaviorSubject.onNext(it)
+                replaySubject.onNext(it)
+
+                if (it == 20L) {
+                    asyncSubject.onComplete()
+                    maybeSubject.onSuccess(it)
+                    singleSubject.onSuccess(it)
+                    completableSubject.onComplete()
+                    dispose?.dispose()
+                }
             }
             .subscribe()
     }
@@ -45,5 +57,19 @@ class RxViewModel() : ViewModel() {
         dispose?.dispose()
     }
 
+    fun observerAsyncSubject(): Observable<Long> {
+        return asyncSubject
+    }
 
+    fun observerPublishSubject(): Observable<Long> {
+        return publishSubject
+    }
+
+    fun observerBehaviorSubject(): Observable<Long> {
+        return behaviorSubject
+    }
+
+    fun observerReplaySubject(): Observable<Long> {
+        return replaySubject
+    }
 }
